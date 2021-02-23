a63cf84e545ae13308b61b41642c5597f39251f6
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-02-23 15:27:45 -0500

    Add input download app link

--- loopqr.sh
+++ loopqr.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "QR Loop Generator"
 read -p "QR_Cant:" qr_cant
+read -p "LinkAppDownload:" link_app
+
+
 
 for (( i = 1; i <= $qr_cant; i++ )) 
 do
-	qrencode -o $i.png "$i"
+	qrencode -o $i.png "$i $link_app"
 done
 
 
